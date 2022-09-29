ed946239200f189f3eccf584629aa5df2fb37ec8
Nicolas Cepeda <nicoecepeda22@gmail.com>
2022-09-28 23:33:32 -0300

    proyecto pizza

--- index.js
+++ index.js
@@ -1,46 +1,61 @@
 const h1 = document.getElementById("nombre");
+const h3 = document.getElementById("ingredientes");
 const h4 = document.getElementById("precio");
 const input = document.querySelector("input");
 const btnEnviar = document.getElementById("enviar");
 const contenedor = document.getElementById("contenedor");
-const pizzas = [
+const cards = document.getElementById("cards")
+let pizzas = [
     {
         id: 1,
         nombre: 'La Muza',
         ingredientes: ['Tomate', 'Mozzarella', 'Condimento'],
-        precio: 1400
+        precio: 1400,
+        images: ["./muza.jpg"]
     },
     {
         id: 2,
         nombre: 'Cebollin',
         ingredientes: ['Tomate', 'Mozzarella', 'Cebolla', 'Jamon'],
-        precio: 1700
+        precio: 1700,
+        images: ["./cebollin.jpg"]
     },
     {
         id: 3,
         nombre: 'SuperQueso',
         ingredientes: ['Tomate', 'Mozzarella', 'Roquefort', 'Cheddar', 'Queso Gourmet'],
-        precio: 2100
+        precio: 2100,
+        images: ["./queso.jpg"]
     },
     {
         id: 4,
         nombre: 'American',
         ingredientes: ['Panceta', 'Cheddar', 'Barbacoa'],
-        precio: 1700
+        precio: 1700,
+        images: ["./american.jpg"]
     },
     {
         id: 5,
         nombre: 'La Especial',
         ingredientes: ['Mozzarella', 'Trucha', 'Anchoas'],
-        precio: 500
+        precio: 500,
+        images: ["./anchoas.jpg"]
     },
     {
         id: 6,
         nombre: 'La Economica',
         ingredientes: ['Tomate', 'Medio Morron'],
-        precio: 350
+        precio: 350,
+        images: ["./economica.jpg"]
     },
 ]
+
+let menupizza = JSON.parse(localStorage.getItem("pizza")) || [];
+
+const saveLocalStorage = () => {
+  localStorage.setItem('pizza', JSON.stringify(menupizza))
+};
+
 btnEnviar.addEventListener("click", enviarId);
 
 function enviarId(e) {
@@ -63,9 +78,15 @@ function enviarId(e) {
 }
 
 const renderPizza = (pizza) => {
-  const { nombre, precio } = pizza; 
-  h2.innerHTML = `<span class= "text-small">Nombre: </span>${nombre}`;
-  h4.innerHTML = `<span class= "text-small">Precio: </span>${precio}`;
+  cards.innerHTML=
+  `
+  <img src=${pizza.images} style="height:190px" alt=${pizza.nombre}>
+      <div class="card-body">
+          <h2>${pizza.nombre}</h2>
+          <h4>$${pizza.precio}</h4>
+          <p>Ingredientes: ${pizza.ingredientes}</p>
+      </div>
+  `
 };
 
 
@@ -85,26 +106,8 @@ const mostrarAlerta = (mensaje) => {
 };
 
 const reset = () => {
-  h2.innerHTML = "";
+  h1.innerHTML = "";
+  h3.innerHTML = "";
   h4.innerHTML = "";
 };
 
-
-// for(let i = 0; i < pizzas.length; i++) {
-//     if(i % 2 === 0) {
-//         console.log (`estas pizzas tienen id: ${pizzas[i].id}`)
-//     }
-// }
-
-// let ValorPizzas = pizzas.filter(pizza => pizza.precio > 600 ?
-//      console.log(`La pizza ${pizza.nombre} esta mas de $600`): 
-//      console.log(`La pizza ${pizza.nombre} cuesta menos de $600`))
-// console.log(ValorPizzas) 
-
-// let nombreYprecio = pizzas.map(pizza => console.log(`La pizza ${pizza.nombre} tiene el precio de $${pizza.precio}`))
-
-// let nombreYingredientes = pizzas.map(function (objeto) {
-//     let ingredientes = objeto.ingredientes
-//     console.log(`La pizza ${objeto.nombre} tiene los ingredientes: ${ingredientes}`)
-
-// }) 
